c409a7ca01dc454d19b2a8ffb60b2ebe1c820ab7
aaronyinyong <aaronyinyong@hotmail.com>
2019-10-22 14:56:45 +0800

    first

---
+++ temp001.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This is a test of markdown.
+
+# In[1]:
+
+
+info=input('give...')
+
+
+# In[2]:
+
+
+print(info)
+
+
+# In[3]:
+
+
+i=1
+type(i)
+
+
+# In[4]:
+
+
+price_str='30.14,29.58,26.36,32.56,32.82'
+type(price_str)
+
+
+# In[5]:
+
+
+id(price_str)
+
+
+# In[6]:
+
+
+from abupy import ABuSymbolPd
+
+
+# 定义一个function，供其他调用
+
+# In[9]:
+
+
+def sub(a,b):
+    return a-b
+
+
+# In[ ]:
+
+
+
+
